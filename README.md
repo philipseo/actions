69c7b46fe20c3dc2915d12c3e7f34bf1642504bc
philipseo <hbnb7894@gmail.com>
2024-01-27 16:41:14 +0900

    test: slack-notify

--- README.md
+++ README.md
@@ -14,13 +14,3 @@
 
 - 재사용 가능한 github action과 workflow를 모아둔 곳 입니다.
 - 실사용 예제는 [philipseo/actions-test](https://github.com/philipseo/actions-test)에서 확인할 수 있습니다.
-
----
-
-### actions
-
----
-
-### workflows
-
----
---
+++ src/slack-notify/slack-notify.ts
@@ -0,0 +1,56 @@
+import core from '@actions/core';
+import github from '@actions/github';
+import { WebClient } from '@slack/web-api';
+
+async function slackNotify() {
+  try {
+    const channelId = core.getInput('channel-id', { required: true });
+    const botToken = core.getInput('bot-token', { required: true });
+    const title = core.getInput('title', { required: true });
+    const extendsSectionFields = core.getInput('extends-section-fields', {
+      required: false,
+    });
+
+    const context = github.context;
+    console.log('aaa', context);
+    // const blocks = [
+    //   {
+    //     type: 'header',
+    //     text: {
+    //       type: 'plain_text',
+    //       text: title,
+    //       emoji: true,
+    //     },
+    //   },
+    //   {
+    //     type: 'section',
+    //     fields: [
+    //       {
+    //         type: 'mrkdwn',
+    //         text: `*Repository*: <${{ github.event.repository.name }}|${{ github.repositoryUrl }}>`,
+    //       },
+    //       {
+    //         type: 'mrkdwn',
+    //         text: `*Branch*: ${{ github.ref_name }}`,
+    //       },
+    //       {
+    //         type: 'mrkdwn',
+    //         text: `*Author*: ${{ github.event.head_commit.author.name }}`,
+    //       }
+    //     ],
+    //   },
+    // ];
+
+    // const slackApi = new WebClient(botToken);
+
+    // await slackApi.chat.postMessage({ channel: channelId, blocks });
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed('Unknown error');
+    }
+  }
+}
+
+export default slackNotify;
