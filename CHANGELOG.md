093b2d84cf2dd76e7056264bb490d86b3f4a3601
philipseo <hbnb7894@gmail.com>
2024-03-11 23:22:49 +0900

    fix: rollback release

--- CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-## v0.1.0 (3/11/2024)
-
----
-
-### Patch Changes
-
----
-
-- Bump Version ([#7](https://github.com/philipseo/actions/pull/7))
--- src/create-github-release-and-tag/create-github-release-and-tag.ts
+++ src/create-github-release-and-tag/create-github-release-and-tag.ts
@@ -4,44 +4,34 @@ import {
   generateReleaseMessage,
   getAllFilePaths,
   getChangedPackagePaths,
-  getPackageJson,
+  getNewVersion,
 } from '#/utils';
 
 async function createGithubReleaseAndTag() {
   const toolkit = new ActionsToolkit();
 
   try {
+    const newVersion = await getNewVersion({
+      prTitle: toolkit.context.pullRequest.title,
+    });
     const packageJsonPaths = await getAllFilePaths({
       filename: 'package.json',
       ignorePatterns: DEFAULT_IGNORE_PATTERNS,
     });
     const changedPackagePaths = await getChangedPackagePaths({ toolkit });
 
-    console.log('aaa', packageJsonPaths, changedPackagePaths);
     for await (const path of packageJsonPaths) {
       const packagePath = path.replace('/package.json', '');
-      const packageJson = await getPackageJson({
-        path,
-      });
-      const version = `v${packageJson.version}`;
       const isChangedPackage = changedPackagePaths.includes(packagePath);
       const releaseMessage = generateReleaseMessage({
         context: toolkit.context,
         isBumpVersion: !isChangedPackage,
       });
-      console.log(
-        'bbb',
-        packagePath,
-        packageJson,
-        version,
-        isChangedPackage,
-        releaseMessage,
-      );
 
       await toolkit.github.repos.createRelease({
         ...toolkit.context.repository,
-        tag_name: version,
-        name: version,
+        tag_name: newVersion,
+        name: newVersion,
         body: releaseMessage,
       });
     }
